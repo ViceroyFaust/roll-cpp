3251abe7bb645bf7f2a69750f0a7b4e841623c6b
Danylo Owl <d.ribchinski@gmail.com>
2021-01-01 21:08:41 -0500

    Set up basic parsing

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@
     int val;
  };
 
+ // returns a line of user input via string
 std::string getUserInput() {
     std::string output;
     std::cout << "> ";
@@ -22,8 +23,48 @@ std::string getUserInput() {
     return output;
 }
 
+// Parses dice notation, ndsOv, eg 2d4+10, and returns a die struct
+die parseNotation(std::string toParse) {
+    std::string stringNum{""}; // String representation of a number before casting
+    die toReturn;
+    enum state { // Tracks the parser's progress
+        NUMBER, SIDES, OPERAND
+    };
+    state progress{NUMBER};
+
+    for (int i = 0; i < toParse.length(); i++) {
+        switch (progress) {
+            case NUMBER:
+                if (toParse.at(i) == 'd') {
+                    toReturn.num = std::stoi(stringNum);
+                    progress = SIDES;
+                    stringNum = "";
+                } else {
+                    stringNum += toParse.at(i);
+                }
+                break;
+            case SIDES:
+                if (toParse.at(i) == '+') {
+                    toReturn.sides = std::stoi(stringNum);
+                    toReturn.oper = toParse.at(i);
+                    stringNum = "";
+                } else {
+                    stringNum += toParse.at(i);
+                }
+                break;
+            case OPERAND:
+                stringNum += toParse.at(i);
+                break;
+        }
+    }
+    toReturn.val = std::stoi(stringNum);
+
+    return toReturn;
+}
+
 int main() {
     std::cout << "Dice Roller" << std::endl;
-    parseNotation(getUserInput());
+    die playerDie = parseNotation(getUserInput());
+    std::cout << playerDie.num << std::endl << playerDie.sides << std::endl << playerDie.oper << std::endl << playerDie.val << std::endl;
     return 0;
 }
